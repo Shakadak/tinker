a8ad659d91e177ed67faaf601611ce1ac99d114d
shakadak <nathanael.pineau@gmail.com>
2016-08-23 15:43:12 +0200

    type added?

--- onlyOddDigPrime.hs
+++ onlyOddDigPrime.hs
@@ -1,13 +1,16 @@
+module Foo where
 import Data.List
- 
+
 onlyOddDigPrime :: Integer -> [Integer]
 onlyOddDigPrime n = [toInteger $ length precedings, preceding, head followings]
     where preceding = if last precedings == n then last $ init precedings else last precedings
           (precedings, followings) = span (<= n) . filter oPrime $ primesST
           oPrime = not . any even . map (read . return) . show
 
+primes :: [Integer]
 primes = [n | n<-[2..], product [1..n-1] `rem` n == n-1] -- Wilson's theorem
 
+primesST :: [Integer]
 primesST = 2 : oddprimes
     where
         oddprimes = sieve 3 9 oddprimes (inits oddprimes)  -- [],[3],[3,5],...
