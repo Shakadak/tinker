3a65fd730f597f008e6b291adbbf81f8dee50087
Nathanaël <Shakadak@users.noreply.github.com>
2016-09-29 18:23:53 +0200

    Update ValidBraces.hs

--- ValidBraces.hs
+++ ValidBraces.hs
@@ -3,6 +3,6 @@ import Data.List
 
 validBraces :: String -> Bool
 validBraces = null . foldl' go []
-    where pairs = ["()", "[]", "{}"]
-          go [] x = [x]
-          go ac x = if [head ac, x] `elem` pairs then tail ac else x:ac
+        where pairs = ["()", "[]", "{}"]
+              go [] x = [x]
+              go ac x = if [head ac, x] `elem` pairs then tail ac else x:ac
